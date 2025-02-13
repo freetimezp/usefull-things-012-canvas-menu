9903a96ed34413cc5de281b015f40fcd1ecd96f6
freetimezp <kraftukrnet@ukr.net>
2025-02-13 21:51:27 +0200

    02. usefull-things-012-canvas-menu. UI. Toggle menu.

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var $page = $('.page');
+
+$('.menu_toggle').on('click', function () {
+    $page.toggleClass('shazam');
+});
+
+$('.content').on('click', function () {
+    $page.removeClass('shazam');
+});
